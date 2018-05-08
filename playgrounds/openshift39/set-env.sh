dac66cbc6ef644a6a131a9d7d5dd31ce90556682
Ferran Castell <ferran.castell@storageos.com>
2018-05-08 09:18:19 +0100

    Updated

--- playgrounds/openshift39/set-env.sh
+++ playgrounds/openshift39/set-env.sh
@@ -1,4 +1,6 @@
 ~/.launch.sh
+curl -kLsSo storageos https://github.com/storageos/go-cli/releases/download/1.0.0-rc1/storageos_linux_amd64 
+chmod +x storageos && mv storageos /usr/local/bin
 [ ! -d /var/lib/storageos ] && (mkdir -p /var/lib/storageos && chmod 0777 /var/lib/storageos)
 echo "Finished launching"
 
