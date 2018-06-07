104b29c1c7bb602403b2bd246c492ecea8ba72c7
Ferran Castell <ferran.castell@storageos.com>
2018-06-07 16:27:41 +0100

    Updated

--- playgrounds/openshift37/set-env.sh
+++ playgrounds/openshift37/set-env.sh
@@ -1,13 +1,13 @@
 ~/.launch.sh
 
-mkdir -p /etc/systemd/system/docker.service.d/
-cat <<EOF > /etc/systemd/system/docker.service.d/clear_mount_propagtion_flags.conf
-[Service]
-MountFlags=shared
-EOF
+#mkdir -p /etc/systemd/system/docker.service.d/
+#cat <<EOF > /etc/systemd/system/docker.service.d/clear_mount_propagtion_flags.conf
+#[Service]
+#MountFlags=shared
+#EOF
 
-export STORAGEOS_USER=storageos STORAGEOS_PASSWORD=storageos
-oc --as system:admin cluster down
-systemctl daemon-reload
-systemctl restart docker.service
-oc --as system:admin cluster up &> /dev/null
+#export STORAGEOS_USER=storageos STORAGEOS_PASSWORD=storageos
+#oc --as system:admin cluster down
+#systemctl daemon-reload
+#systemctl restart docker.service
+#oc --as system:admin cluster up &> /dev/null
